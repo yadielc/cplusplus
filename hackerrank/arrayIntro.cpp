c080049a082e419293b00614e6ade8e0262112b0
Yadiel <ycabr035@fiu.edu>
2018-07-31 15:58:49 -0400

    added comments

--- hackerrank/arrayIntro.cpp
+++ hackerrank/arrayIntro.cpp
@@ -5,6 +5,16 @@
 #include <algorithm>
 using namespace std;
 
+/* Array Introductions: Hacker Rank Problem solution
+
+Description:
+This is my solution to the exercise on HackerRank for Introduction to
+arrays.
+
+The problem was to display the items on reverse order
+from a given array.
+*/
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
@@ -15,21 +25,16 @@ int main() {
 
     int a;
 
-
-
     // fill array with numbers
     for (int i = 0; i < n; i++){
           cin >> a;
           array[i] = a;
     }
 
-
-
     // display the numbers in the reverse order
     for(int i = n-1; i>=0; i--){
-       cout << array[i] << " " << endl;
+       cout << array[i] << " ";
     }
-
 
 
     return 0;
